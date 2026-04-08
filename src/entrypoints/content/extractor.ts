1e1e8f0747487697e9a4a2630578cd92f5903c5c
Hugo Marotta <humarotta@proton.me>
2026-04-08 16:32:37 -0300

    Add function to get artist name from page

--- src/entrypoints/content/extractor.ts
+++ src/entrypoints/content/extractor.ts
@@ -11,3 +11,12 @@ export function getTitle(): string {
 
   return el.textContent?.trim() ?? "";
 }
+
+export function getArtist(): string {
+  const el = document.querySelector("#js-lyric-content h2");
+  if (!el) {
+    return "";
+  }
+
+  return el.textContent?.trim() ?? "";
+}
