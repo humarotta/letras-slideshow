802db279f6cc14505f33eea67e6d3d456f3e182a
Hugo Marotta <humarotta@proton.me>
2026-04-09 23:59:34 -0300

    Add message listener to start presentation

--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,10 +1,12 @@
 import { getLyrics } from "./extractor";
+import { startPresentation } from "./presenter";
 
 export default defineContentScript({
   matches: ["*://*.letras.com/*", "*://*.letras.mus.br/*"],
   main() {
     browser.runtime.onMessage.addListener((message, _, sendResponse) => {
       if (message.type === "GET_LYRICS") sendResponse(getLyrics());
+      if (message.type === "START_PRESENTATION") startPresentation(getLyrics());
     });
   },
 });
