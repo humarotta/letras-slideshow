1c19e94ba85ccc9374b65991996bda6d03afee6d
Hugo Marotta <humarotta@proton.me>
2026-04-10 22:52:15 -0300

    Add WXT I18n

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   srcDir: "src",
   outDir: ".out",
-  modules: ["@wxt-dev/auto-icons"],
+  modules: ["@wxt-dev/auto-icons", "@wxt-dev/i18n/module"],
 });
