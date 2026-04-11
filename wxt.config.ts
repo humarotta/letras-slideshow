7456d11efd5fe11dc8cc66daf515ced370ca75db
Hugo Marotta <humarotta@proton.me>
2026-04-10 23:08:53 -0300

    Set extension name

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   outDir: ".out",
   modules: ["@wxt-dev/auto-icons", "@wxt-dev/i18n/module"],
   manifest: {
+    name: "__MSG_extension_name__",
     default_locale: "en",
   },
 });
