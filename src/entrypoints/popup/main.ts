cd67c4055c41c4791e123714c9cb4343d919334e
Hugo Marotta <humarotta@proton.me>
2026-04-13 22:31:18 -0300

    Add function to get typed elements by ID

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,5 +1,9 @@
 import { type GeneratedI18nStructure, i18n } from "#i18n";
 
+function getElementById<T extends HTMLElement>(id: string): T {
+  return document.getElementById(id) as T;
+}
+
 function localize() {
   for (const el of document.querySelectorAll("[data-i18n]")) {
     const key = el.getAttribute("data-i18n") as keyof GeneratedI18nStructure;
