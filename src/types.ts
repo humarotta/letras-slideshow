3299ff97eb14880f77be00cd406310d019f32fbe
Hugo Marotta <humarotta@proton.me>
2026-04-17 17:24:24 -0300

    Add song domain model

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,14 @@ export interface Lyrics {
   artist: string;
   stanzas: string[][];
 }
+
+export type Song = SongMeta & SongLyrics;
+
+export type SongMeta = {
+  title: string;
+  artist: string;
+};
+
+export type SongLyrics = {
+  stanzas: string[][];
+};
