fdda3c9cfd9a4ffb4625b8472be38f3d1fbf3f71
Hugo Marotta <humarotta@proton.me>
2026-04-18 20:51:47 -0300

    Allow custom root in selector functions

--- src/utils/selector.ts
+++ src/utils/selector.ts
@@ -1,5 +1,9 @@
-export const $ = <T extends HTMLElement>(selector: string): T =>
-  document.querySelector<T>(selector) as T;
+export const $ = <T extends HTMLElement>(
+  selector: string,
+  root: ParentNode = document,
+): T => root.querySelector<T>(selector) as T;
 
-export const $$ = <T extends HTMLElement>(selector: string): NodeListOf<T> =>
-  document.querySelectorAll<T>(selector);
+export const $$ = <T extends HTMLElement>(
+  selector: string,
+  root: ParentNode = document,
+): NodeListOf<T> => root.querySelectorAll<T>(selector);
