1ea724edf2b1c193b0017e04ab0300f86ca7feb2
Hugo Marotta <humarotta@proton.me>
2026-04-18 17:12:34 -0300

    Add function to get all matching elements

--- src/utils/selector.ts
+++ src/utils/selector.ts
@@ -1,2 +1,5 @@
 export const $ = <T extends HTMLElement>(selector: string): T =>
   document.querySelector<T>(selector) as T;
+
+export const $$ = <T extends HTMLElement>(selector: string): NodeListOf<T> =>
+  document.querySelectorAll<T>(selector);
