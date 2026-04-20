b3adf9816a0e0ae2320d0ca4d14903d3730be0fb
Hugo Marotta <humarotta@proton.me>
2026-04-20 11:34:00 -0300

    Add documentation to types module

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,27 @@
+/**
+ * A song, combining its metadata and lyrics.
+ */
 export type Song = SongMeta & SongLyrics;
 
+/**
+ * The metadata that identifies a song.
+ */
 export type SongMeta = {
   title: string;
   artist: string;
 };
 
+/**
+ * The lyrics of a song, structured as a two-dimensional array of stanzas and
+ * their verses.
+ */
 export type SongLyrics = {
   stanzas: string[][];
 };
 
+/**
+ * The message types used to communicate with the content script.
+ */
 export enum MessageType {
   GetSongMeta = "GET_SONG_META",
   StartPresentation = "START_PRESENTATION",
